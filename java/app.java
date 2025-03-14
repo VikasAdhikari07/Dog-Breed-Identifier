df0d91618c424b9de8bbd2cfaf29721cdd7528ca
VikasAdhikari07 <91743784+VikasAdhikari07@users.noreply.github.com>
2025-03-14 21:09:01 +0530

    Delete java directory

--- java/app.java
+++
@@ -1,9 +0,0 @@
-import java.applet.*;
-import java.awt.*;
-
-public class app extends Applet
-{
-    public void paint(Graphics g) {
-        g.drawString("Hello World",100, 100);
-    }
-}
--- java/hashing/hashmap.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-public class hashmap {
-    public static void main(String[] args) {
-        Scanner scr = new Scanner(System.in);
-        String str = scr.next();
-            
-    }
-}
